bd879132109e1542230ff58fa0080d9a2d578375
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-20 17:41:37 -0500

    Test dev commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search" />
-    <p>search term: {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue';
+import { ref } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
-    const search = ref('');
-    const names = ref([
-      'Alex Carrilo',
-      'Ethan Trevino',
-      'Jeffrey-James Turner',
-      'Kyle Hernandez',
-      'Nixon Blankenship',
-      'Nolan Blankenship',
+    const posts = ref([
+      { title: 'Welcome to the blog ', body: 'Lorem ipsum', id: 1 },
+      { title: 'Top 5 css tips ', body: 'Lorem ipsum', id: 2 },
     ]);
 
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran');
-    });
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value);
-    });
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    });
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    };
-
-    return { names, search, matchingNames, handleClick };
+    return { posts };
   },
 };
 </script>
