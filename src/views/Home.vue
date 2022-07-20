5fc5089fdac620bdc2e8f734349f74acb53e8d87
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-20 15:17:00 -0500

    Got the names to show up "before filter"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,18 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">Add 1 to age</button>
-    <input type="text" v-model="name" />
+    <div v-for="name in names" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { computed, ref } from '@vue/reactivity';
 
 export default {
   name: 'Home',
   setup() {
-    // const p = ref('hello');
-
-    const name = ref('Kyle');
-    const age = ref(16);
-
-    const handleClick = () => {
-      name.value = 'Kate';
-      age.value = 13;
-    };
-
-    return {
-      name,
-      age,
-      handleClick,
-    };
-  },
-  data() {
-    return {
-      score: 5,
-    };
+    const names = ref(['alex', 'ethan', 'j.j', 'kyle', 'nixon', 'nolan']);
+    return { names };
   },
 };
 </script>
